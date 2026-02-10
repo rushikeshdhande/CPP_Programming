fadb1af26444666427578fee57b81e6f19e128f1
Rushikesh Dhande <161755491+rushikeshdhande@users.noreply.github.com>
2026-02-10 23:09:18 +0530

    Update program281.cpp

--- Classwork/Day_03/program281.cpp
+++ Classwork/Day_03/program281.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 void Display()
 {
     cout<<"Jay Ganesh...\n";
@@ -8,10 +7,8 @@ void Display()
     cout<<"Jay Ganesh...\n";
     cout<<"Jay Ganesh...\n";
 }
-
 int main()
 {
     Display();
-
     return 0;
 }
